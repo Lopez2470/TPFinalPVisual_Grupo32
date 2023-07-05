85dacac64f4fb990f5dce80b3f7b8b6cd3eb291c
Carlos <Carlos Lopez>
2023-07-05 01:28:18 -0300

    Creacion de la capa service de entidad Usuario.

---
+++ TPFinal_Grupo32/src/main/java/ar/edu/unju/fi/service/IUsuarioService.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import ar.edu.unju.fi.entity.Usuario;
+
+public interface IUsuarioService {
+	/**
+	 * Definicion de metodos abstractos
+	 */
+	
+	List <Usuario> getUsuarios();
+	
+	Usuario getUsuario();
+	
+	public void guardarUsuario(Usuario usuario);
+	
+	public Usuario getBy(Long id);
+
+}
