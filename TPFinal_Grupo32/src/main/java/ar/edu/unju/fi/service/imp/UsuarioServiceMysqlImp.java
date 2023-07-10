9baa585579f6a409ad775cb36b3dc70f43d8b75f
GabrielaLopez <GTL@DESKTOP-0LQB8T5>
2023-07-10 17:19:19 -0300

    Documentacion de Usuario: Documentacion de capas de servicio, implementacion, controlador.

--- TPFinal_Grupo32/src/main/java/ar/edu/unju/fi/service/imp/UsuarioServiceMysqlImp.java
+++ TPFinal_Grupo32/src/main/java/ar/edu/unju/fi/service/imp/UsuarioServiceMysqlImp.java
@@ -11,35 +11,49 @@ import ar.edu.unju.fi.service.IUsuarioService;
 
 @Service
 public class UsuarioServiceMysqlImp implements IUsuarioService{
-	
+	/*
+	 * Inyeccion de service
+	 */
 	@Autowired
 	private IUsuarioRepository usuarioRepository;
-	
+	/*
+	 * Inyeccion de la entidad Usuario
+	 */
 	@Autowired
 	private Usuario usuario;
-
+	/*
+	 * Metodo que devuelve todos los usuarios que sean unicamente
+	 * Usuarios comunes
+	 */
 	@Override
 	public List<Usuario> getUsuarios() {
 		return usuarioRepository.findByAdmin(false);
 	}
-
+	/*
+	 * Metodo que nos devuelve un objeto Usuario
+	 */
 	@Override
 	public Usuario getUsuario() {
 		return usuario;
 	}
-
+	/*
+	 * Metodo que guarda el usuario pasado como parametro
+	 */
 	@Override
 	public void guardarUsuario(Usuario usuario) {
 		usuarioRepository.save(usuario);
-		
-		
 	}
-
+	/*
+	 * Metodo que devuelve un objeto Usuario a partir
+	 * de su nro de id pasado como parametro
+	 */
 	@Override
 	public Usuario getBy(Long id) {
 		return usuarioRepository.findById(id).get();
 	}
-
+	/*
+	 * Metodo que busca si existe un usuarui a partir de su id
+	 */
 	@Override
 	public Boolean encontrarUsuario(Long id) {
 		return usuarioRepository.existsById(id);
